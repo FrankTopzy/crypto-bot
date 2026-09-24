ae7cbac5c724ec678a3d73b6802854b84048e7b9
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2026-09-24 04:46:25 -0800

    feat: also updated the t_bot file.

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,26 @@
 import requests
 
-from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+from config import (
+    TELEGRAM_BOT_TOKEN,
+    TELEGRAM_CHAT_ID
+)
 
 
 def send_message(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    url = (
+        f"https://api.telegram.org/"
+        f"bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    )
 
     data = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": message,
     }
 
-    response = requests.post(url, json=data)
+    response = requests.post(
+        url,
+        json=data
+    )
 
     return response.json()
-
-send_message("Hello!")
