5f4f85fb395bce54d005bfa15de4bf4504d6beb8
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2026-09-23 23:39:07 -0800

    feat: this file handles how the bot behaves.

---
+++ telegram_bot.py
@@ -0,0 +1,18 @@
+import requests
+
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
+
+
+def send_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+    }
+
+    response = requests.post(url, json=data)
+
+    return response.json()
+
+send_message("Hello!")
