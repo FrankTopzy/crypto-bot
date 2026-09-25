998b68eba42ef09360f31cea6a8f34119a3260b3
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-09-25 16:40:26 +0100

    chore: remove production pipeline tes

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-from reddit_monitor import process_post, start_monitor
-
-
-test_post = {
-    "id": "RAILWAY_FULL_PIPELINE_TEST_001",
-    "title": "I can't withdraw my BTC from Coinbase",
-    "body": "I have tried several times but my Bitcoin withdrawal is stuck.",
-    "subreddit": "coinbase",
-    "author": "railway_test_user",
-    "url": "https://www.reddit.com/r/coinbase/",
-}
+from reddit_monitor import start_monitor
 
 
 if __name__ == "__main__":
-    process_post(test_post)
-
     start_monitor()
