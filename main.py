a2bf8b04f77f538063808010a98bb57631789ae2
FrankTopzy <temitopeadeoye787898@gmail.com>
2026-09-25 16:35:03 +0100

    feat: test completed

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from reddit_monitor import start_monitor
-from telegram_bot import send_message
 
 
 if __name__ == "__main__":
-    send_message(
-        "🚨 RAILWAY TEST\n\n"
-        "Crypto Reddit Alert Bot is running successfully on Railway."
-    )
-
     start_monitor()
